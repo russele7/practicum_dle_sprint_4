20f510fcfcf68e3046707b0d1aa78c2d8429a172
russele7 <russele7oge@gmail.com>
2025-12-03 15:01:26 +0300

    upd2

--- src/dataset.py
+++ src/dataset.py
@@ -1,9 +1,11 @@
 
 import re
+import random
 
+import pandas as pd
 from PIL import Image
 import albumentations as A
-
+import numpy as np
 from torch.utils.data import Dataset
 import timm
 from transformers import AutoTokenizer
@@ -13,30 +15,40 @@ import torch
 
 class MultimodalDataset(Dataset):
     def __init__(self,
-                 df,
-                 text_model,
-                 image_model,
+                 config,
                  transforms,
-                 dict_ingredients):
+                 mode,):
+        if mode not in ['train', 'test']:
+            raise ValueError(f'mode = {mode}')
+        df = pd.read_csv(f'{DATA_PATH}dish.csv')
+        self.df = df[df['split'] == mode].reset_index(drop=True)
+
         self.df = df
-        self.image_cfg = timm.get_pretrained_cfg(image_model)
-        self.tokenizer = AutoTokenizer.from_pretrained(text_model)
+        self.image_cfg = timm.get_pretrained_cfg(config.IMAGE_MODEL_NAME)
+        self.tokenizer = AutoTokenizer.from_pretrained(config.TEXT_MODEL_NAME)
         self.transform = transforms
-        self.dict_ingredients = dict_ingredients
+
+        df_ing = pd.read_csv(f'{DATA_PATH}ingredients.csv')
+        self.dict_ingredients = {k: v for k, v in
+                                 zip(df_ing['id'], df_ing['ingr'])}
 
     def __len__(self):
         return len(self.df)
 
-    def __getitem__(self, idx):        
+    def __getitem__(self, idx):
 
-        ingredients_idx = self.df.loc[idx, "ingredients_idx"]
+        # ingredients_idx = 
+        ingredients_idx = get_ingredients_idx(self.df.loc[idx, "ingredients"])
+
         text = ', '.join([self.dict_ingredients[idx]
                           for idx in ingredients_idx])
 
         dish_id = self.df.loc[idx, "dish_id"]
         img_path = f'{DATA_PATH}images/{dish_id}/rgb.png'
         image = Image.open(img_path).convert('RGB')
-        image = self.transform(image)
+        # print(f'type(image) = {type(image)}')
+        # print(f'image.shape = {image.shape}')
+        image = self.transform(image=np.array(image))["image"]
 
         total_mass = self.df.loc[idx, "total_mass"]
 
@@ -51,11 +63,28 @@ class MultimodalDataset(Dataset):
             "total_mass": total_mass,
         }
     
-def collate_fn(batch, tokenizer):
+def get_ingredients_idx(raw_text):
+    return [int(t[12:]) for t in raw_text.split(';')]
+
+    
+def text_interfusion(text):
+    text_list = text.split(", ")
+    random.shuffle(text_list)
+    return ", ".join(text_list) 
+
+
+def texts_interfusion(texts):
+    texts_lists = [t.split(", ") for t in texts]
+    for tl in texts_lists:
+        random.shuffle(tl)
+    return [", ".join(t) for t in texts_lists]
+
+
+def collate_fn(batch, tokenizer, mode):
     texts = [item["text"] for item in batch]
 
-    # if text_augmentation:
-    #texts = [text_interfusion(text) for text in texts]
+    if mode == 'train':
+        texts = [text_interfusion(text) for text in texts]
 
     total_masses = torch.LongTensor([item["total_mass"] for item in batch])
 
@@ -68,9 +97,55 @@ def collate_fn(batch, tokenizer):
                                 padding="max_length",
                                 truncation=True)
     return {
-        "target": targets,            
+        "target": targets,
         "image": images,
         "input_ids": tokenized_input["input_ids"],
         "attention_mask": tokenized_input["attention_mask"],
         "total_mass": total_masses,
     }
+
+
+
+def get_transforms(config, mode="train"):
+    cfg = timm.get_pretrained_cfg(config.IMAGE_MODEL_NAME)
+
+    if mode == "train":
+        transforms = A.Compose(
+            [
+                A.SmallestMaxSize(
+                    max_size=max(cfg.input_size[1], cfg.input_size[2]), p=1.0),
+                A.RandomCrop(
+                    height=cfg.input_size[1], width=cfg.input_size[2], p=1.0),
+                A.Affine(scale=(0.8, 1.2),
+                        rotate=(-15, 15),
+                        translate_percent=(-0.1, 0.1),
+                        shear=(-10, 10),
+                        fill=0,
+                        p=0.8),
+                A.CoarseDropout(num_holes_range=(2, 8),
+                                hole_height_range=(int(0.07 * cfg.input_size[1]),
+                                                int(0.15 * cfg.input_size[1])),
+                                hole_width_range=(int(0.1 * cfg.input_size[2]),
+                                                int(0.15 * cfg.input_size[2])),
+                                fill=0,
+                                p=0.5),
+                A.ColorJitter(
+                    brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1, p=0.7),
+                A.Normalize(mean=cfg.mean, std=cfg.std),
+                A.ToTensorV2(p=1.0)
+            ],
+            seed=42,
+        )
+    else:
+        transforms = A.Compose(
+            [
+                A.SmallestMaxSize(
+                    max_size=max(cfg.input_size[1], cfg.input_size[2]), p=1.0),
+                A.CenterCrop(
+                    height=cfg.input_size[1], width=cfg.input_size[2], p=1.0),
+                A.Normalize(mean=cfg.mean, std=cfg.std),
+                A.ToTensorV2(p=1.0)
+            ]
+        )
+
+    return transforms
--- src/utils.py
+++ src/utils.py
@@ -1,73 +1,33 @@
 
+import os
 import re
+import random
 from random import shuffle
+from functools import partial
 
-from PIL import Image
 import albumentations as A
-
-from torch.utils.data import Dataset
+import numpy as np
 import timm
 from transformers import AutoTokenizer
-from src.constants import DATA_PATH
+from torch.utils.data import DataLoader
+# from src.constants import DATA_PATH
+from tqdm import tqdm
+import torchmetrics
 import torch
+import torch.nn as nn
+from torch.optim import AdamW
+from transformers import AutoModel, AutoTokenizer
+
+from .dataset import MultimodalDataset, get_transforms, collate_fn
 
 
-def text_interfusion(text):
-    text_list = text.split(", ")
-    shuffle(text_list)
-    return ", ".join(text_list) 
-
-
-def texts_interfusion(texts):
-    texts_lists = [t.split(", ") for t in texts]
-    for tl in texts_lists:
-        shuffle(tl)
-    return [", ".join(t) for t in texts_lists]
-
-
-def get_transforms(config, ds_type="train"):
-    cfg = timm.get_pretrained_cfg(config.IMAGE_MODEL_NAME)
-
-    if ds_type == "train":
-        transforms = A.Compose(
-            [
-                A.SmallestMaxSize(
-                    max_size=max(cfg.input_size[1], cfg.input_size[2]), p=1.0),
-                A.RandomCrop(
-                    height=cfg.input_size[1], width=cfg.input_size[2], p=1.0),
-                A.Affine(scale=(0.8, 1.2),
-                        rotate=(-15, 15),
-                        translate_percent=(-0.1, 0.1),
-                        shear=(-10, 10),
-                        fill=0,
-                        p=0.8),
-                A.CoarseDropout(num_holes_range=(2, 8),
-                                hole_height_range=(int(0.07 * cfg.input_size[1]),
-                                                int(0.15 * cfg.input_size[1])),
-                                hole_width_range=(int(0.1 * cfg.input_size[2]),
-                                                int(0.15 * cfg.input_size[2])),
-                                fill=0,
-                                p=0.5),
-                A.ColorJitter(
-                    brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1, p=0.7),
-                A.Normalize(mean=cfg.mean, std=cfg.std),
-                A.ToTensorV2(p=1.0)
-            ],
-            seed=42,
-        )
-    else:
-        transforms = A.Compose(
-            [
-                A.SmallestMaxSize(
-                    max_size=max(cfg.input_size[1], cfg.input_size[2]), p=1.0),
-                A.CenterCrop(
-                    height=cfg.input_size[1], width=cfg.input_size[2], p=1.0),
-                A.Normalize(mean=cfg.mean, std=cfg.std),
-                A.ToTensorV2(p=1.0)
-            ]
-        )
-
-    return transforms
+def seed_everything(seed: int):
+    random.seed(seed)
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.benchmark = True
 
 
 def set_requires_grad(module, unfreeze_pattern="", verbose=False):
@@ -85,3 +45,184 @@ def set_requires_grad(module, unfreeze_pattern="", verbose=False):
                 print(f"Разморожен слой: {name}")
         else:
             param.requires_grad = False
+
+
+class MultimodalModel(nn.Module):
+    def __init__(self, config,):
+        super().__init__()
+        # self.hidden_state = emb_dim
+        self.text_model = AutoModel.from_pretrained(config.TEXT_MODEL_NAME)
+        self.image_model = timm.create_model(
+            config.IMAGE_MODEL_NAME,
+            pretrained=True,
+            num_classes=0
+        )
+
+        self.regressor = nn.Sequential(
+            nn.Sequential(
+                nn.Linear(
+                    in_features=(
+                        self.text_model.config.hidden_size
+                        + self.image_model.num_features
+                        + 1
+                    ),
+                    out_features=config.HIDDEN_DIM,
+                    bias=True
+                ),
+                nn.LayerNorm(config.HIDDEN_DIM),
+                nn.Hardswish(),
+                nn.Dropout(p=config.DROPOUT),
+                nn.Linear(in_features=config.HIDDEN_DIM,
+                          out_features=1, bias=True),
+            )
+        )
+
+    def forward(self, text_input, image_input, mass_input):
+        text_features = self.text_model(
+            **text_input).last_hidden_state[:,  0, :]
+        image_features = self.image_model(image_input)
+        mass_features = torch.tensor(mass_input).unsqueeze(1)
+
+        # print(text_features.shape, image_features.shape, mass_features.shape)
+        multi_output = torch.cat(
+            [text_features, image_features, mass_features], dim=1)
+        # print(multi_output.shape)
+        output = self.regressor(multi_output)
+        # print(output.shape)
+        return output
+
+
+def train(config, device):
+    seed_everything(config.SEED)
+
+    # Инициализация модели
+    model = MultimodalModel(config).to(device)
+    tokenizer = AutoTokenizer.from_pretrained(config.TEXT_MODEL_NAME)
+
+    set_requires_grad(model.text_model,
+                      unfreeze_pattern=config.TEXT_MODEL_UNFREEZE, verbose=True)
+    set_requires_grad(model.image_model,
+                      unfreeze_pattern=config.IMAGE_MODEL_UNFREEZE, verbose=True)
+
+    # Оптимизатор с разными LR
+    optimizer = AdamW([{
+        'params': model.text_model.parameters(),
+        'lr': config.TEXT_LR
+    }, {
+        'params': model.image_model.parameters(),
+        'lr': config.IMAGE_LR
+    }, {
+        'params': model.regressor.parameters(),
+        'lr': config.REGRESSOR_LR
+    }])
+
+    criterion = nn.L1Loss()
+
+    # Загрузка данных
+    transforms = get_transforms(config, mode="train")
+    val_transforms = get_transforms(config, mode="test")
+    train_dataset = MultimodalDataset(config, transforms, mode="train")
+    val_dataset = MultimodalDataset(config, val_transforms, mode="test")
+    train_loader = DataLoader(train_dataset,
+                              batch_size=config.BATCH_SIZE,
+                              shuffle=True,
+                              collate_fn=partial(collate_fn,
+                                                 tokenizer=tokenizer,
+                                                 mode="train"))
+    val_loader = DataLoader(val_dataset,
+                            batch_size=config.BATCH_SIZE,
+                            shuffle=False,
+                            collate_fn=partial(collate_fn,
+                                               tokenizer=tokenizer,
+                                               mode="test"))
+
+    # инициализируем метрику
+    torchmetrics.MeanAbsoluteError()
+
+    mae_metric_train = torchmetrics.MeanAbsoluteError().to(device)
+    mae_metric_val = torchmetrics.MeanAbsoluteError().to(device)
+    # best_mae_train = 0.0
+    best_mae_val = 0.0
+
+    print("training started")
+    for epoch in range(config.EPOCHS):
+        model.train()
+        total_loss = 0.0
+        print(f'epoch {epoch}')
+        for batch in tqdm(train_loader):
+            # Подготовка данных
+            # inputs = {
+            #     'input_ids': batch['input_ids'].to(device),
+            #     'attention_mask': batch['attention_mask'].to(device),
+            #     'image': batch['image'].to(device),
+            #     'total_mass': batch['total_mass'].to(device)
+            # }
+            inputs = {
+                'text_input': {'input_ids': batch['input_ids'].to(device),
+                               'attention_mask': batch['attention_mask'].to(device)},
+                'image_input': batch['image'].to(device),
+                'mass_input': batch['total_mass'].to(device)
+            }
+            # labels = batch['label'].to(device)
+            targets = batch['target'].to(device)
+
+            # forward(self, text_input, image_input, mass_input)
+
+
+            # Forward
+            optimizer.zero_grad()
+            preds = model(**inputs).squeeze()
+            loss = criterion(preds, targets)
+
+            # Backward
+            loss.backward()
+            optimizer.step()
+
+            total_loss += loss.item()
+
+            _ = mae_metric_train(preds=preds, target=targets)
+
+        # Валидация
+        train_mae = mae_metric_train.compute().cpu().numpy()
+        val_mae = validate(model, val_loader, device, mae_metric_val)
+        mae_metric_val.reset()
+        mae_metric_train.reset()
+
+        print(
+            f"Epoch {epoch}/{config.EPOCHS-1} | "
+            f"avg_Loss: {total_loss/len(train_loader):.4f} | "
+            f"Train MAE: {train_mae :.4f}| "
+            f"Val MAE: {val_mae :.4f}"
+        )
+
+        if val_mae > best_mae_val:
+            print(f"New best model, epoch: {epoch}")
+            best_mae_val = val_mae
+            torch.save(model.state_dict(), config.SAVE_PATH)
+
+
+def validate(model, val_loader, device, metric):
+    model.eval()
+
+    with torch.no_grad():
+        for batch in tqdm(val_loader):
+
+            # inputs = {
+            #     'input_ids': batch['input_ids'].to(device),
+            #     'attention_mask': batch['attention_mask'].to(device),
+            #     'image': batch['image'].to(device),
+            #     'total_mass': batch['total_mass'].to(device)
+            # }
+            inputs = {
+                'text_input': {'input_ids': batch['input_ids'].to(device),
+                               'attention_mask': batch['attention_mask'].to(device)},
+                'image_input': batch['image'].to(device),
+                'mass_input': batch['total_mass'].to(device)
+            }
+
+            targets = batch['target'].to(device)
+
+            preds = model(**inputs).squeeze()
+            _ = metric(preds=preds, target=targets)
+
+    return metric.compute().cpu().numpy()
